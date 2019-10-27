e585cd57ec4dc5c03d451e62f8132a3c7e13fc8f
Steffen Cornwell <steffencornwell@gmail.com>
2019-10-27 17:42:40 -0400

    Broken Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { 
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from 'react-router-dom';
 import './static/styles/App.scss';
+import OrganizationSignup from './components/OrganizationSignup';
+import ClientSignup from './components/ClientSignup';
+import Login from './components/Login';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Router>
+      	<Switch>
+      		<Route exact path="/">
+      			<Login />
+      		</Route>
+      		<Route path="/organization-signup">
+      			<OrganizationSignup />
+      		</Route>
+      		<Route path="/client-signup">
+      			<ClientSignup />
+      		</Route>
+      	</Switch>
+      </Router>
     </div>
   );
 }
