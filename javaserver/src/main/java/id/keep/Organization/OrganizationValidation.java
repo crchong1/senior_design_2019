dd286d13f1b774196329acc808d5c6245a4d2e5a
jalbi <jbigbee@seas.upenn.edu>
2019-11-07 04:36:40 -0500

    Some Logging and validation

---
+++ javaserver/src/main/java/id/keep/Organization/OrganizationValidation.java
@@ -0,0 +1,82 @@
+package id.keep.Organization;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import Regex.ValRegex;
+import id.keep.Log.Log;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+
+public class OrganizationValidation {
+	public static boolean valOrg(Context ctx) throws SecurityException, IOException{
+		Log l = new Log();
+		String fn = ctx.formParam("firstname");
+        String ln = ctx.formParam("lastname");
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
+        String org = ctx.formParam("orgName");	
+        
+        //First Name
+        if (fn == null || fn.strip().length() == 0) {
+        	l.logger.warning("First Name Empty");
+        	return false;
+        }       
+        if (fn.length() > 30 || fn.length() < 2) {
+        	l.logger.warning("First Name Inapopriate Length " + fn.length());
+        	return false;
+        }
+        if (!Pattern.matches(ValRegex.name, fn)) {
+        	l.logger.warning("Invalid Characters used In First Name"); //Should I include
+        	return false;
+        }
+
+        //Last Name
+        if (ln == null || ln.strip().length() == 0) {
+        	l.logger.warning("Last Name Empty");	
+        	return false;
+        }
+        if (ln.length() > 30 || ln.length() < 2) {
+        	l.logger.warning("Last Name Inapopriate Length " + fn.length());
+        	return false;
+        }
+        if (!Pattern.matches(ValRegex.name, ln)) {
+        	l.logger.warning("Invalid Characters Used In Last Name"); //Should I include
+        	return false;
+        }
+        
+        //Email
+        if (email == null || email.strip().length() == 0) {
+        	l.logger.warning("Email Empty");
+        	return false;
+        }
+        if (email.length() > 40) {
+        	l.logger.warning("Email Is Too Long " + email.length());
+        	return false;
+        }
+        if (!Pattern.matches(ValRegex.email, email)) {
+        	l.logger.warning("Email Uses Invalid Characters");  
+        	return false;
+        }
+        
+        //Password
+        if (password == null || password.strip().length() == 0) {
+        	l.logger.warning("Password Empty");
+        	return false;
+        }
+        if (password.length() < 7) {
+        	l.logger.warning("Password is less than 7 characters");
+        	return false;
+        }
+        
+        //Organization Name
+        if (org == null || org.strip().length() == 0) {
+        	l.logger.warning("Orginization is Empty");
+        	return false;
+        }
+        if (!Pattern.matches(ValRegex.org, org)) {
+        	l.logger.warning("Orginization Name Contains Invalid Characters");
+        	return false;
+        }
+        return true;
+    }
+}
