1f304514369c3f41f2c5a65851aa6b40ac87d221
Gregory Kofman <kofmangregory@gmail.com>
2019-11-07 01:52:53 -0500

    Deleted javascript server

--- server/app/errorMessages.ts
+++
@@ -1,6 +0,0 @@
-enum Message {
-  AUTH_SUCCESS = 'Successfully Authenticated User',
-  AUTH_FAILURE = 'Wrong Credentials',
-  HASH_FAILURE = 'Error: Check Argon2 documentation',
-}
-export default Message;
