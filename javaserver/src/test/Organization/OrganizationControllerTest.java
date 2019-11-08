60d027677e71ce929cb0732613a60c92ac47c324
Gregory Kofman <kofmangregory@gmail.com>
2019-11-08 14:13:52 -0500

    Send form entries to lowercase

--- javaserver/src/test/Organization/OrganizationControllerTest.java
+++ javaserver/src/test/Organization/OrganizationControllerTest.java
@@ -1,14 +1,27 @@
 package Organization;
 
+import com.github.fakemongo.Fongo;
+import io.javalin.http.Context;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class OrganizationControllerTest {
 
     @BeforeEach
     void setUp() {
+        Fongo fongo = new Fongo("Fongo Server");
+    }
+
+    @Test
+    void signUpValidOrg() {
+        Context ctx = mock(Context.class);
+        Mockito.when(ctx.formParam("firstname")).thenReturn("Greg");
     }
 
     @AfterEach
