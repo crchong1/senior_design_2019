8c8bea96f6e23747b2c977cbc94d0441b16d0d23
Gregory Kofman <kofmangregory@gmail.com>
2019-11-12 12:13:49 -0500

    Started testing with Mockito and Fongo. NullPointerExcpetions thrown due to Context object not having headers set properly

--- javaserver/src/test/Organization/OrganizationControllerTest.java
+++ javaserver/src/test/Organization/OrganizationControllerTest.java
@@ -13,15 +13,33 @@ import static org.mockito.Mockito.mock;
 
 class OrganizationControllerTest {
 
+    private Context ctx;
+
     @BeforeEach
     void setUp() {
         Fongo fongo = new Fongo("Fongo Server");
+        ctx = mock(Context.class);
     }
 
     @Test
     void signUpValidOrg() {
-        Context ctx = mock(Context.class);
-        Mockito.when(ctx.formParam("firstname")).thenReturn("Greg");
+        System.out.println("CONTEXT: ");
+        System.out.println(ctx);
+
+        Mockito.when(ctx.formParam("firstname")).thenReturn("fname");
+        Mockito.when(ctx.formParam("lastname")).thenReturn("lname");
+        Mockito.when(ctx.formParam("birthdate")).thenReturn("1/1/2000");
+        Mockito.when(ctx.formParam("email")).thenReturn("xxx@xxx.com");
+        Mockito.when(ctx.formParam("password")).thenReturn("badpassword");
+        Mockito.when(ctx.formParam("orgName")).thenReturn("nameoforg");
+
+        try {
+            OrganizationController.enrollOrganization.handle(ctx);
+            Mockito.verify(ctx).json(OrgEnrollmentStatus.SUCCESSFUL_ENROLLMENT);
+        }
+        catch (Exception e) {
+            fail("Exception thrown.");
+        }
     }
 
     @AfterEach
