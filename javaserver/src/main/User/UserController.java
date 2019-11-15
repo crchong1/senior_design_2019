0b3e01ef0f4ed25bcc1566674f978aa15c61d719
jalbi <jbigbee@seas.upenn.edu>
2019-11-14 19:02:39 -0500

    Some work on password recovery

--- javaserver/src/main/User/UserController.java
+++ javaserver/src/main/User/UserController.java
@@ -1,5 +1,4 @@
 package User;
-
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -9,11 +8,66 @@ import de.mkammerer.argon2.Argon2Factory;
 import Config.MongoConfig;
 import io.javalin.http.Handler;
 import org.bson.Document;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+import secerts.*;
+
+import java.util.Properties;
 
 public class UserController {
     public static Handler loginUser = ctx -> {
         ctx.result(loginAuthenticate(ctx.formParam("username"), ctx.formParam("password")).getErrorName());
     };
+
+    public static Handler recoverPassword = ctx -> {
+        ctx.result(recoverPassword(ctx.formParam("username")).getErrorName());
+    };
+
+
+    private static UserMessage recoverPassword(String username) {
+        String host = "xxxxxxx@xxx.xxx";
+        MongoClient client = MongoConfig.getMongoClient();
+        MongoDatabase database = client.getDatabase(MongoConfig.getDatabaseName());
+        MongoCollection<Document> userCollection = database.getCollection("user");
+        Document user = userCollection.find(eq("username", username)).first();
+        String to = null;
+        if (user == null) {
+            return UserMessage.USER_NOT_FOUND;
+        }
+        to = GetEmail(user);
+        if (to == null){
+            return UserMessage.NO_EMAIL_ATTACHED;
+        }
+        // look up database
+        // generate unique hash of a random number
+        return UserMessage.AUTH_FAILURE;
+    };
+    private static String GetEmail(Document user){
+        return user.get("email", String.class);
+    }
+    private static boolean Send(String to, String from){
+        Properties prop = System.getProperties();
+        String host = "localhost";
+        prop.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+        try{
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("This is the Subject Line!");
+            message.setText("This is actual message");
+            Transport.send(message);
+            return true;
+        }
+        catch{
+            return false;
+        }
+    }
+    private static String GenText(String userName){
+        String token = secerts.token_urlsafe(16);
+        return "";
+    }
 
     private static UserMessage loginAuthenticate(String username, String password) {
         Argon2 argon2 = Argon2Factory.create();
--- javaserver/src/main/User/UserMessage.java
+++ javaserver/src/main/User/UserMessage.java
@@ -4,7 +4,9 @@ public enum UserMessage {
     AUTH_SUCCESS("AUTH_SUCCESS:Successfully Authenticated User"),
     AUTH_FAILURE("AUTH_FAILURE:Wrong Credentials"),
     HASH_FAILURE("HASH_FAILURE:Check Argon2 documentation"),
-    USER_NOT_FOUND("USER_NOT_FOUND:User does not exist in database.");
+    USER_NOT_FOUND("USER_NOT_FOUND:User does not exist in database."),
+    NO_EMAIL_ATTACHED("NO_EMAIL_ATTACHED:Their is no email attached to given user.");
+
 
     public String errorMessage;
 
