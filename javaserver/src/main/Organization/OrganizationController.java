07640ced4f79316eac89b40d60780f05059826c0
Gregory Kofman <kofmangregory@gmail.com>
2019-11-21 14:27:37 -0500

    Organization signup uses correct formParams and sets session token

--- javaserver/src/main/Organization/OrganizationController.java
+++ javaserver/src/main/Organization/OrganizationController.java
@@ -18,44 +18,59 @@ public class OrganizationController {
 
         MongoCollection<Document> orgCollection = database.getCollection("organization");
         Document existingOrg = orgCollection.find(eq("orgName", orgName)).first();
-        
+
         if (existingOrg != null) {
             ctx.json(OrgEnrollmentStatus.ORG_EXISTS);
         }
         else {
-            String adminFirstname = ctx.formParam("firstname").toLowerCase();
-            String adminLastname = ctx.formParam("lastname").toLowerCase();
-            String birthdate = ctx.formParam("birthdate");
-            String adminUsername = adminFirstname + "-" + adminLastname
-                    + "-" + orgName + "-" + birthdate;
-            String adminEmail = ctx.formParam("email").toLowerCase();
+            String orgWebsite = ctx.formParam("orgWebsite").toLowerCase();
+            String adminName = ctx.formParam("name").toLowerCase();
+            String orgContactPhoneNumber = ctx.formParam("phone").toLowerCase();
+            String email = ctx.formParam("email").toLowerCase();
+            String username = ctx.formParam("username");
+            String password = ctx.formParam("password");
+            String address = ctx.formParam("address").toLowerCase();
+            String city = ctx.formParam("city").toLowerCase();
+            String state = ctx.formParam("state").toUpperCase();
+            String zipcode = ctx.formParam("zipcode");
+            String taxCode = ctx.formParam("taxCode");
+            Integer numUsers = Integer.parseInt(ctx.formParam("numUsers"));
+
 
             Argon2 argon2 = Argon2Factory.create();
-            char[] password = ctx.formParam("password").toCharArray();
+            char[] passwordArr = password.toCharArray();
             String passwordHash;
             try {
-                passwordHash = argon2.hash(10, 65536, 1, password);
-                argon2.wipeArray(password);
+                passwordHash = argon2.hash(10, 65536, 1, passwordArr);
+                argon2.wipeArray(passwordArr);
             }
             catch (Exception e) {
-                argon2.wipeArray(password);
+                argon2.wipeArray(passwordArr);
                 ctx.json(OrgEnrollmentStatus.PASS_HASH_FAILURE);
                 return;
             }
 
             MongoCollection<Document> userCollection = database.getCollection("user");
-            Document newAdmin = new Document("username", adminUsername)
+            Document newAdmin = new Document("username", username)
                     .append("password", passwordHash)
                     .append("organization", orgName)
-                    .append("email", adminEmail)
-                    .append("firstname", adminFirstname)
-                    .append("lastname", adminLastname)
-                    .append("birthdate", birthdate)
-                    .append("usertype", "admin");
+                    .append("email", email)
+                    .append("name", adminName)
+                    .append("privelegeLevel", "admin");
             userCollection.insertOne(newAdmin);
 
-            Document newOrg = new Document("orgName", orgName);
+            Document newOrg = new Document("orgName", orgName)
+                    .append("website", orgWebsite)
+                    .append("contact number", orgContactPhoneNumber)
+                    .append("street address", address)
+                    .append("city", city)
+                    .append("state", state)
+                    .append("zipcode", zipcode)
+                    .append("taxCode", taxCode)
+                    .append("expectedNumUsers", numUsers);
             orgCollection.insertOne(newOrg);
+
+            ctx.sessionAttribute("privelegeLevel", "admin");
             ctx.json(OrgEnrollmentStatus.SUCCESSFUL_ENROLLMENT);
         }
     };
